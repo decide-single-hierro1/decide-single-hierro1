627087379d28c1c61ef342166e076696f7d190ff
javhidrod8 <48653257+javhidrod8@users.noreply.github.com>
2022-11-24 16:09:56 +0100

    fix: Uncomment some tests
    Uncomment some tests to check CI
    
    commit co-authors: ignrodflo

--- decide/visualizer/tests.py
+++ decide/visualizer/tests.py
@@ -4,15 +4,15 @@ import visualizer.views as view
 class VisualizerTests(BaseTestCase):
     # data = ['visualizer/migrations/prueba.json', ]
     
-    # def setUp(self):
-    #     super().setUp()
+    def setUp(self):
+        super().setUp()
 
-    # def tearDown(self):
-    #     super().tearDown()
+    def tearDown(self):
+        super().tearDown()
         
-    # def test_vista_visualizer(self):
-    #    response = self.client.get('/visualizer/')
-    #    self.assertEqual(response.status_code, 200)
+    def test_vista_visualizer(self):
+       response = self.client.get('/visualizer/')
+       self.assertEqual(response.status_code, 200)
 
     # def test_votacion_existente(self):
     #     response = self.client.get('/visualizer/1/')
