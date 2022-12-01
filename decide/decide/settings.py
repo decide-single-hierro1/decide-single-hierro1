a5fb04667db48cff1b21403239b6ee7a1438ce29
IJordano <n.jordano10@gmail.com>
2022-12-01 13:20:30 +0100

    feat: Add django-telegrambot dependency and settings
    Add django-telegrambot dependecy, a python-telegram-bot wrapper for django
    which allows easy setup as a webhook. Requires changes in settings:
    -Add django-telegrambot urls in decide/urls.py
    -Add new settings section in decide/settings.py

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^##ydkswfu0+=ofw0l#$kv^8n)0$i(qd&d&ol#p9!b$8*5%j1+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_framework_swagger',
     'gateway',
+    'django_telegrambot',
     'telegram_bot',
 ]
 
@@ -162,8 +163,70 @@ STATIC_URL = '/static/'
 # number of bits for the key, all auths should use the same number of bits
 KEYBITS = 256
 
-#token del bot de telegram
+# Telegram Bot Token
 TELEGRAM_APIKEY_BOT = '5896257584:AAH2krGA7MZuMRrPd-mrAiSDv2TCWiQ5PCM'
+
+# Django Telegram Bot settings
+DJANGO_TELEGRAMBOT = {
+
+    'MODE' : 'WEBHOOK', #(Optional [str]) # The default value is WEBHOOK,
+                        # otherwise you may use 'POLLING'
+                        # NB: if use polling you must provide to run
+                        # a management command that starts a worker
+
+    # !!!!!! Set to deployment Server !!!!!!
+    # For testing, run >> ngrok http 8000 and paste forwarding server
+    'WEBHOOK_SITE' : 'https://<change.this>.eu.ngrok.io',
+    'WEBHOOK_PREFIX' : '/prefix', # (Optional[str]) # If this value is specified,
+                                  # a prefix is added to webhook url
+
+    #'WEBHOOK_CERTIFICATE' : 'cert.pem', # If your site use self-signed
+                         #certificate, must be set with location of your public key
+                         #certificate.(More info at https://core.telegram.org/bots/self-signed )
+
+    'BOTS' : [
+        {
+           'TOKEN': '5896257584:AAH2krGA7MZuMRrPd-mrAiSDv2TCWiQ5PCM', #Your bot token.
+
+           #'ALLOWED_UPDATES':(Optional[list[str]]), # List the types of
+                                                   #updates you want your bot to receive. For example, specify
+                                                   #``["message", "edited_channel_post", "callback_query"]`` to
+                                                   #only receive updates of these types. See ``telegram.Update``
+                                                   #for a complete list of available update types.
+                                                   #Specify an empty list to receive all updates regardless of type
+                                                   #(default). If not specified, the previous setting will be used.
+                                                   #Please note that this parameter doesn't affect updates created
+                                                   #before the call to the setWebhook, so unwanted updates may be
+                                                   #received for a short period of time.
+
+           #'TIMEOUT':(Optional[int|float]), # If this value is specified,
+                                   #use it as the read timeout from the server
+
+           #'WEBHOOK_MAX_CONNECTIONS':(Optional[int]), # Maximum allowed number of
+                                   #simultaneous HTTPS connections to the webhook for update
+                                   #delivery, 1-100. Defaults to 40. Use lower values to limit the
+                                   #load on your bot's server, and higher values to increase your
+                                   #bot's throughput.
+
+           #'POLL_INTERVAL' : (Optional[float]), # Time to wait between polling updates from Telegram in
+                           #seconds. Default is 0.0
+
+           #'POLL_CLEAN':(Optional[bool]), # Whether to clean any pending updates on Telegram servers before
+                                   #actually starting to poll. Default is False.
+
+           #'POLL_BOOTSTRAP_RETRIES':(Optional[int]), # Whether the bootstrapping phase of the `Updater`
+                                   #will retry on failures on the Telegram server.
+                                   #|   < 0 - retry indefinitely
+                                   #|     0 - no retries (default)
+                                   #|   > 0 - retry up to X times
+
+           #'POLL_READ_LATENCY':(Optional[float|int]), # Grace time in seconds for receiving the reply from
+                                   #server. Will be added to the `timeout` value and used as the read timeout from
+                           #server (Default: 2).
+        },
+        #Other bots here with same structure.
+    ],
+}
 
 # Versioning
 ALLOWED_VERSIONS = ['v1', 'v2']
