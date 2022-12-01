a4f154f372eb4df68a7572cea728b1282958bfe6
javhidrod8 <48653257+javhidrod8@users.noreply.github.com>
2022-12-01 19:23:54 +0100

    feat: API URLs for metrics methods
    Created new urls that will be used in telegram bot
    
    commit co-authors: ignrodflo

--- decide/visualizer/urls.py
+++ decide/visualizer/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
 from .views import VisualizerView, VisualizerQuestion,  VisualizerAll
+from .metrics import listVotes, votesOfVoting, unstartedVotings, startedVotings, finishedVotings, closedVotings, votingComparator, abstentions
 
 
 urlpatterns = [
     path('<int:voting_id>/', VisualizerView.as_view()),
     path('',VisualizerAll.as_view()),
     path('question/<int:question_id>/',VisualizerQuestion.as_view()),
+    path('list', listVotes),
+    path('votes', votesOfVoting),
+    path('unstarted', unstartedVotings),
+    path('started', startedVotings),
+    path('finished', finishedVotings),
+    path('closed', closedVotings),
+    path('comparator/<int:v1_id>/<int:v2_id>', votingComparator),
+    path('abstentions/<int:v_id>', abstentions),
 ]
