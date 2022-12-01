0da56f6d8e7c8448830295d060ec4718300b2d62
DESKTOP-F9MOQ6O\Mallup <javierhidalgo@easyleapp.com>
2022-12-01 21:15:03 +0100

    feat: API routes for metrics methods
    Changed API route structure to get metrics methods from views.py
    
    commit co-authors: ignrodflo

--- decide/visualizer/metrics.py
+++ decide/visualizer/metrics.py
@@ -3,8 +3,8 @@ from voting.models import Voting, Question
 from census.models import Census
 from django.shortcuts import get_list_or_404
 
-def listVotes():
-    return Vote.objects.all()
+def listVotings():
+    return Voting.objects.all()
 
 def votesOfVoting(v_id):
     return Vote.objects.filter(voting_id = v_id).count()
@@ -24,6 +24,8 @@ def closedVotings():
 def votingComparator(v1_id, v2_id):
     v1 = votesOfVoting(v1_id)
     v2 = votesOfVoting(v2_id)
+    if(v1 == 0 or v2 == 0):
+        return 0
     return (v1/v2)*100
 
 def abstentions(v_id):
--- decide/visualizer/urls.py
+++ decide/visualizer/urls.py
@@ -1,18 +1,16 @@
 from django.urls import path
-from .views import VisualizerView, VisualizerQuestion,  VisualizerAll
-from .metrics import listVotes, votesOfVoting, unstartedVotings, startedVotings, finishedVotings, closedVotings, votingComparator, abstentions
-
+from .views import VisualizerView, VisualizerQuestion,  VisualizerAll, VisualizerList, FinishedVotings, VotesOfVoting, UnstartedVotings, StartedVotings, ClosedVotings, VotingComparator, Abstentions
 
 urlpatterns = [
     path('<int:voting_id>/', VisualizerView.as_view()),
     path('',VisualizerAll.as_view()),
     path('question/<int:question_id>/',VisualizerQuestion.as_view()),
-    path('list', listVotes),
-    path('votes', votesOfVoting),
-    path('unstarted', unstartedVotings),
-    path('started', startedVotings),
-    path('finished', finishedVotings),
-    path('closed', closedVotings),
-    path('comparator/<int:v1_id>/<int:v2_id>', votingComparator),
-    path('abstentions/<int:v_id>', abstentions),
+    path('list', VisualizerList.as_view()),
+    path('votes/<int:v_id>', VotesOfVoting.as_view()),
+    path('unstarted', UnstartedVotings.as_view()),
+    path('started', StartedVotings.as_view()),
+    path('finished', FinishedVotings.as_view()),
+    path('closed', ClosedVotings.as_view()),
+    path('comparator/<int:v1_id>/<int:v2_id>', VotingComparator.as_view()),
+    path('abstentions/<int:v_id>', Abstentions.as_view()),
 ]
